03a50fa06bc36cc9c6a9a34f9b4ba0f480ffe8e7
Pingis2 <anton@worksit.se>
2025-02-26 11:06:21 +0100

    updated name from "type" to "field"

--- functions.php
+++ functions.php
@@ -117,17 +117,17 @@ function awesome_custom_taxonomies() {
 
     // add new taxonomy hierarchical
     $labels = array(
-        'name' => 'Types',
-        'singular_name' => 'Type',
-        'search_items' => 'Search Types',
-        'all_items' => 'All Types',
-        'parent_item' => 'Parent Type',
-        'parent_item_colon' => 'Parent Type:',
-        'edit_item' => 'Edit Type',
-        'update_item' => 'Update Type',
-        'add_new_item' => 'Add New Type',
-        'new_item_name' => 'New Type Name',
-        'menu_name' => 'Type'
+        'name' => 'Fields',
+        'singular_name' => 'Field',
+        'search_items' => 'Search Fields',
+        'all_items' => 'All Fields',
+        'parent_item' => 'Parent Field',
+        'parent_item_colon' => 'Parent Field:',
+        'edit_item' => 'Edit Field',
+        'update_item' => 'Update Field',
+        'add_new_item' => 'Add New Field',
+        'new_item_name' => 'New Field Name',
+        'menu_name' => 'Field'
     );
 
     $args = array(
@@ -136,10 +136,10 @@ function awesome_custom_taxonomies() {
         'show_ui' => true,
         'show_admin_column' => true,
         'query_var' => true,
-        'rewrite' => array('slug' => 'type')
+        'rewrite' => array('slug' => 'field')
     );
 
-    register_taxonomy('type', array('portfolio'), $args);
+    register_taxonomy('field', array('portfolio'), $args);
 
     // add new taxonomy NOT hierarchical
 }
