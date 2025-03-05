f5228467f1f94749272be767f9fd1272d86e54e5
Pingis2 <anton@worksit.se>
2025-03-05 13:18:09 +0100

    added button and updated home page

--- functions.php
+++ functions.php
@@ -228,3 +228,15 @@ function aweseme_get_terms($postID, $term) {
 
     return $output;
 }
+
+function remove_max_srcset_image_width( $max_width ) {
+    return false;
+}
+
+add_filter( 'max_srcset_image_width', 'remove_max_srcset_image_width' );
+
+function wdo_disable_srcset( $sources ) {
+    return false;
+}
+
+add_filter( 'wp_calculate_image_srcset', 'wdo_disable_srcset' );
